0b9cbc87f432a948b68e2cb3309d9dead7d6d0d8
Nicholas Gosemick <n.gosemick@gmail.com>
2020-10-22 11:45:05 -0400

    final

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
     //Un commentaire
     console.log("Ça fonctionne!!!");
 
+    var hamburger = document.querySelector('.hamburger-simple');
+
+    hamburger.addEventListener("click", openMenu);
+
+    function openMenu(evt){
+
+        console.log("click");
+    }
 });
